2c3665400c7f16815224142111dcdab625e09067
j4ceu <sutan.raihan@ui.ac.id>
2022-11-11 00:01:57 +0700

    fix: Port in Heroku

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"Mini-Project_Coaching-Clinic/configs"
 	"Mini-Project_Coaching-Clinic/route"
+	"os"
 )
 
 func main() {
 	configs.Init()
 
-	e := route.NewRouter()           // Init Router
-	e.Logger.Fatal(e.Start(":8080")) // Start Server
+	e := route.NewRouter()                     // Init Router
+	e.Logger.Fatal(e.Start(os.Getenv("PORT"))) // Start Server
 
-	
 }
